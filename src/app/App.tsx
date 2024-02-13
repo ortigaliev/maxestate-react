0e607de14e0a909eb66cb56b4dfae29438dea949
ortigaliev <ortigaliyevm@gmail.com>
2024-02-13 22:21:53 +0900

    feat: Material UI custom instalation for the project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,9 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 import "../css/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <Container></Container>;
 }
 
 export default App;
