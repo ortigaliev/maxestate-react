71db86b60a38c2000e28dd204e008b0d67e100f9
ortigaliev <ortigaliyevm@gmail.com>
2024-02-14 11:00:08 +0900

    feat: Develop header and other pages routing logic

--- src/app/components/headers/agency.tsx
+++ src/app/components/headers/agency.tsx
@@ -1,7 +1,109 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function NavbarAgency(props: any) {
   return (
-  <div>ANavbar Agency</div>
+    <div className="format_agency home_navbar">
+      <Container>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config agency_navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/images/agency/logo_black.png" alt="Logo" />
+          </Box>
+          <Stack
+            className="navbar_links"
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+          >
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/agency" activeClassName="underline">
+                Agency
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/property" activeClassName="underline">
+                Property
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/blog" activeClassName="underline">
+                Blog
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/contact" activeClassName="underline">
+                Contact
+              </NavLink>
+            </Box>
+            {/* Shopping Cart  */}
+            <Box className="agency_hover-line hover-line ">
+              <IconButton
+                aria-label="cart"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-haspopup="true"
+                aria-expanded={undefined}
+                //onClick={handleClick}
+              />
+              <Badge badgeContent={3} color="secondary">
+                <Box className="agency_hover-line_icon-box">
+                  <img
+                    className="agency_hover-line_icon"
+                    src="/images/home/shopping_cart.svg"
+                    alt="Shopping Cart"
+                  />
+                </Box>
+              </Badge>
+            </Box>
+            <Box>
+              <Button
+                className=" agency_hover-line hover-line"
+                variant="contained"
+                style={{
+                  width: "160px",
+                  height: "56px",
+                  backgroundColor: "#ff5a3c",
+                }}
+              >
+                SIGN IN
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+      <div className="format_agency_header">
+        <Box className="format_agency_img"></Box>
+      </div>
+    </div>
   );
 }
--- src/app/components/headers/others.tsx
+++ src/app/components/headers/others.tsx
@@ -1,5 +1,111 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function NavbarOthers(props: any) {
-  return (<div>NavbarOthers</div>);
+  return (
+    <div className="format_others home_navbar">
+      <Container style={{ height: "116px" }}>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config others_navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/images/agency/logo_black.png" alt="Logo" />
+          </Box>
+          <Stack
+            className="navbar_links"
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+          >
+            <Box
+              className="others_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box
+              className="others_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/agency" activeClassName="underline">
+                Agency
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/property" activeClassName="underline">
+                Property
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/blog" activeClassName="underline">
+                Blog
+              </NavLink>
+            </Box>
+            <Box
+              className="agency_hover-line hover-line"
+              onClick={props.setPath}
+            >
+              <NavLink to="/contact" activeClassName="underline">
+                Contact
+              </NavLink>
+            </Box>
+            {/* Shopping Cart  */}
+            <Box className="others_hover-line hover-line ">
+              <IconButton
+                aria-label="cart"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-haspopup="true"
+                aria-expanded={undefined}
+                //onClick={handleClick}
+              />
+              <Badge badgeContent={3} color="secondary">
+                <Box className="others_hover-line_icon-box">
+                  <img
+                    className="others_hover-line_icon"
+                    src="/images/home/shopping_cart.svg"
+                    alt="Shopping Cart"
+                  />
+                </Box>
+              </Badge>
+            </Box>
+            <Box>
+              <Button
+                className=" others_hover-line hover-line"
+                variant="contained"
+                style={{
+                  width: "160px",
+                  height: "56px",
+                  backgroundColor: "#ff5a3c",
+                }}
+              >
+                SIGN IN
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+      <div className="format_others_header">
+        <Box className="format_others_img">
+          {/* <img src="/images/home/others_bg.jpg" alt="" /> */}
+        </Box>
+      </div>
+    </div>
+  );
 }
