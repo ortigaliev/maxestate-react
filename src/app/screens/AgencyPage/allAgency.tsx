8e43c0d390780bc704457ea310cd1b2e158c6e31
ortigaliev <ortigaliyevm@gmail.com>
2024-05-12 22:03:25 +0900

    fix: mapping agencies for allAgency screen

--- src/app/screens/AgencyPage/allAgency.tsx
+++ src/app/screens/AgencyPage/allAgency.tsx
@@ -20,6 +20,9 @@ import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
 
+const order_list = Array.from(Array(6).keys());
+console.log(order_list);
+
 export function AllAgency() {
   return (
     <div className="all_agency">
@@ -28,7 +31,6 @@ export function AllAgency() {
           <CssVarsProvider>
             {/* CARD SECTION */}
             <Stack>
-              {/* NEW CARD 1 */}
               <Typography
                 mb={5}
                 component="h2"
@@ -40,81 +42,102 @@ export function AllAgency() {
               >
                 All Agencies
               </Typography>
-              <Card
-                variant="outlined"
-                sx={{
-                  width: 400,
-                  // to make the card resizable
-                  overflow: "auto",
-                  resize: "horizontal",
-                }}
-              >
-                <CardOverflow>
-                  <AspectRatio ratio="1.3">
-                    <img
-                      src="/images/agency/luxhouse.png"
-                      loading="lazy"
-                      alt="news_img"
-                    />
-                  </AspectRatio>
-                </CardOverflow>
-                <CardContent>
-                  <Typography level="title-lg" mb={2}>
-                    A&A Realty Co
-                  </Typography>
-                  <Stack sx={{ marginBottom: "15px" }} flexDirection={"row"}>
-                    <LocationOnIcon
-                      sx={{
-                        display: "block",
-                        width: 25,
-                        height: 30,
-                        fontSize: 20,
-                        color: "#ff5a3c",
-                      }}
-                    />
-                    <Typography sx={{ color: "#5c727d" }} level="body-lg">
-                      3, Sejong-ro 2-gil
-                    </Typography>
-                  </Stack>
-                  <Stack sx={{ marginBottom: "15px" }} flexDirection={"row"}>
-                    <LocalPhoneOutlinedIcon
-                      sx={{
-                        display: "block",
-                        width: 25,
-                        height: 30,
-                        fontSize: 20,
-                        color: "#ff5a3c",
-                      }}
-                    />
-                    <Typography sx={{ color: "#5c727d" }} level="body-lg">
-                      01093717177
-                    </Typography>
-                  </Stack>
-                </CardContent>
-                <CardOverflow
-                  variant="soft"
-                  sx={{ bgcolor: "background.level1" }}
-                >
-                  <CardActions buttonFlex="0 0 auto">
-                    <IconButton variant="outlined" color="neutral">
-                      <div>12k</div>
-                      <FavoriteBorder />
-                    </IconButton>
-                    <Divider orientation="vertical" />
-                    <IconButton
-                      variant="outlined"
-                      color="neutral"
-                      sx={{ mr: "auto" }}
+            </Stack>
+            <Stack
+              flexDirection={"row"}
+              flexWrap={"wrap"}
+              display={"flex"}
+              height={"auto"}
+              gap={2}
+              sx={{ position: "relative", mb: 5 }}
+            >
+              {/* NEW CARD 1 */}
+
+              {order_list.map((ele) => {
+                return (
+                  <Card
+                    variant="outlined"
+                    sx={{
+                      width: 400,
+                      // to make the card resizable
+                      overflow: "auto",
+                      resize: "horizontal",
+                    }}
+                  >
+                    <CardOverflow>
+                      <AspectRatio ratio="1.3">
+                        <img
+                          src="/images/agency/luxhouse.png"
+                          loading="lazy"
+                          alt="news_img"
+                        />
+                      </AspectRatio>
+                    </CardOverflow>
+                    <CardContent>
+                      <Typography level="title-lg" mb={2}>
+                        A&A Realty Co
+                      </Typography>
+                      <Stack
+                        sx={{ marginBottom: "15px" }}
+                        flexDirection={"row"}
+                      >
+                        <LocationOnIcon
+                          sx={{
+                            display: "block",
+                            width: 25,
+                            height: 30,
+                            fontSize: 20,
+                            color: "#ff5a3c",
+                          }}
+                        />
+                        <Typography sx={{ color: "#5c727d" }} level="body-lg">
+                          3, Sejong-ro 2-gil
+                        </Typography>
+                      </Stack>
+                      <Stack
+                        sx={{ marginBottom: "15px" }}
+                        flexDirection={"row"}
+                      >
+                        <LocalPhoneOutlinedIcon
+                          sx={{
+                            display: "block",
+                            width: 25,
+                            height: 30,
+                            fontSize: 20,
+                            color: "#ff5a3c",
+                          }}
+                        />
+                        <Typography sx={{ color: "#5c727d" }} level="body-lg">
+                          01093717177
+                        </Typography>
+                      </Stack>
+                    </CardContent>
+                    <CardOverflow
+                      variant="soft"
+                      sx={{ bgcolor: "background.level1" }}
                     >
-                      <div>10k</div>
-                      <RemoveRedEyeOutlinedIcon />
-                    </IconButton>
-                    <Button variant="solid" sx={{ bgcolor: "#ff5a3c" }}>
-                      Follow
-                    </Button>
-                  </CardActions>
-                </CardOverflow>
-              </Card>
+                      <CardActions buttonFlex="0 0 auto">
+                        <IconButton variant="outlined" color="neutral">
+                          <div>12k</div>
+                          <FavoriteBorder />
+                        </IconButton>
+                        <Divider orientation="vertical" />
+                        <IconButton
+                          variant="outlined"
+                          color="neutral"
+                          sx={{ mr: "auto" }}
+                        >
+                          <div>10k</div>
+                          <RemoveRedEyeOutlinedIcon />
+                        </IconButton>
+                        <Button variant="solid" sx={{ bgcolor: "#ff5a3c" }}>
+                          Follow
+                        </Button>
+                      </CardActions>
+                    </CardOverflow>
+                  </Card>
+                );
+              })}
             </Stack>
           </CssVarsProvider>
         </Stack>
